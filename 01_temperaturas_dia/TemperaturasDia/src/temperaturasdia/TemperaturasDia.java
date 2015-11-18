0c9aa951f2da091cbdfc07ca6ad97f496cb5cc2d
AitorBM <aitorbringas@gmail.com>
2015-11-18 19:32:14 +0100

    Ejercicio 1 terminado

--- 01_temperaturas_dia/TemperaturasDia/src/temperaturasdia/TemperaturasDia.java
+++ 01_temperaturas_dia/TemperaturasDia/src/temperaturasdia/TemperaturasDia.java
@@ -23,14 +23,23 @@ public class TemperaturasDia {
         // TODO code application logic here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        Double[] temperaturas = new Double[24];
+        //Double[] temperaturas = new Double[24];
         
+        double[] temperaturas = {
+            15.0, 15.2, 15.1, 15.2,
+            15.1, 15.1, 14.9, 16.1,
+            16.5, 17.2, 19.5, 20.0,
+            22.1, 23.3, 25.2, 26.3,
+            25.9, 25.8, 24.2, 24.1,
+            20.7, 19.6, 17.3, 16.
+        };
+        /*
         for (int i = 0; i < 24; i++)
         {
             System.out.print("Introduce la temperatura: ");
             temperaturas[i] = Double.parseDouble(br.readLine());
         }
-        
+        */
         double max = temperaturas[0];
         double min = temperaturas[0];
         double media = 0;
@@ -53,5 +62,29 @@ public class TemperaturasDia {
         media /= 24;
         
         System.out.println("max: "+max+" min: "+min+" media: "+media);
+        
+        for (int j = 0; j < 24; j++)
+        {
+            System.out.print(j+" ");
+        
+            for (int i = 0; i < temperaturas[j]; i++)
+            {
+                System.out.print("*");
+            }
+        
+            System.out.print(" "+temperaturas[j]);
+            
+            if (temperaturas[j] == max)
+            {
+                System.out.print(" --> MAX");
+            }
+            if (temperaturas[j] == min)
+            {
+                System.out.print(" --> MIN");
+            }
+            
+            System.out.println();
+        }
+        
     }
 }
