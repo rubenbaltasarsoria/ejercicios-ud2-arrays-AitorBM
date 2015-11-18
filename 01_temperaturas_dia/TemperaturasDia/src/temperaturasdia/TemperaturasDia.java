2111ccc022251c8d1018666ec80afd66fc9a2467
AitorBM <aitorbringas@gmail.com>
2015-11-18 19:05:22 +0100

    Ejercicio 1 hace los calculos

--- 01_temperaturas_dia/TemperaturasDia/src/temperaturasdia/TemperaturasDia.java
+++ 01_temperaturas_dia/TemperaturasDia/src/temperaturasdia/TemperaturasDia.java
@@ -5,17 +5,53 @@
  */
 package temperaturasdia;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
- * @author widemos
+ * @author AitorBM
  */
 public class TemperaturasDia {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Double[] temperaturas = new Double[24];
+        
+        for (int i = 0; i < 24; i++)
+        {
+            System.out.print("Introduce la temperatura: ");
+            temperaturas[i] = Double.parseDouble(br.readLine());
+        }
+        
+        double max = temperaturas[0];
+        double min = temperaturas[0];
+        double media = 0;
+        
+        for (int i = 0; i < 24; i++)
+        {
+            if (max < temperaturas[i])
+            {
+                max = temperaturas[i];
+            }
+            
+            if (min > temperaturas[i])
+            {
+                min = temperaturas[i];
+            }
+            
+            media = media + temperaturas[i];
+        }
+        
+        media /= 24;
+        
+        System.out.println("max: "+max+" min: "+min+" media: "+media);
     }
-    
 }
