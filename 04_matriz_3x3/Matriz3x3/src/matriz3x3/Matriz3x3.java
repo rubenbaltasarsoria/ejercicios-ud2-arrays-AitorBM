e9c38e51891819ce04e73f1c581faef4b8dc0793
AitorBM <aitorbringas@gmail.com>
2015-11-18 21:59:13 +0100

    Ejercicio 4 terminado

--- 04_matriz_3x3/Matriz3x3/src/matriz3x3/Matriz3x3.java
+++ 04_matriz_3x3/Matriz3x3/src/matriz3x3/Matriz3x3.java
@@ -6,6 +6,7 @@
 package matriz3x3;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -16,8 +17,9 @@ public class Matriz3x3 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
@@ -27,8 +29,17 @@ public class Matriz3x3 {
         {
             for (int j = 0; j < 3; j++)
             {
-                System.out.println("Introduce el valor "+i+","+j+": ");
+                System.out.print("Introduce el valor "+i+","+j+": ");
+                matriz[i][j] = Integer.parseInt(br.readLine());
             }
         }
+        
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.format("%d %d %d", matriz[i][0],matriz[i][1],matriz[i][2]);
+            System.out.println("");
+        }
+        
+        
     }
 }
