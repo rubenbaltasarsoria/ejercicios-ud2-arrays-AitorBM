6a95b5cfca462ad6091f21ed203fd271c07e640d
AitorBM <aitorbringas@gmail.com>
2015-11-18 20:52:16 +0100

    Ejercicio 2 terminado

--- 02_fecha_texto/FechaTexto/src/fechatexto/FechaTexto.java
+++ 02_fecha_texto/FechaTexto/src/fechatexto/FechaTexto.java
@@ -5,17 +5,54 @@
  */
 package fechatexto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
- * @author widemos
+ * @author AitorBM
  */
 public class FechaTexto {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] dias = { 
+            "uno", "dos", "tres", 
+            "cuatro", "cinco", "seis", 
+            "siete", "ocho", "nueve", 
+            "diez", "once", "doce", 
+            "trece", "catorce", 
+            "quince", "dieciséis", 
+            "diecisiete", "dieciocho", 
+            "diecinueve", "veinte", 
+            "veintiuno", "veintidós", 
+            "veintitrés", "veinticuatro", 
+            "veinticinco", "veintiseis", 
+            "veintisiete", "veintiocho", 
+            "veintinueve", "treinta"};
+        
+        String[] meses = {
+            "enero", "febrero", "marzo", 
+            "abril", "mayo", "junio", 
+            "julio", "agosto", "septiembre",
+            "octubre", "noviembre", "diciembre"};
+        
+        System.out.print("Introduce la fecha en formato DD/MM/2015: ");
+        String fechaN = br.readLine();
+        
+        String[] fechasN = fechaN.split("/");
+        
+        int diaN = Integer.parseInt(fechasN[0]);
+        int mesN = Integer.parseInt(fechasN[1]);
+        int anoN = Integer.parseInt(fechasN[2]);
+        
+        System.out.print(dias[diaN-1]+" de "+meses[mesN-1]+" de dos mil quince.");
     }
-    
 }
